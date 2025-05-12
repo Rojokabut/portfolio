140254a06f6a7bd43c5e8c3dabb1ae7f084d5dd7
Rojo <rojoniainaravelomanana@gmail.com>
2025-05-12 21:09:29 +0300

    nesorina le navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Github , Menu} from 'lucide-react'
+import { Github } from 'lucide-react'
 import Link from 'next/link'
 
 export default function Navbar() {
@@ -8,7 +8,7 @@ export default function Navbar() {
     <div className='lg:hidden fixed w-full z-10 overflow-hidden'>
       <div className='flex items-center justify-between bg-[#0f172a] px-3 py-1 w-full rounded-b-lg'>
         <div className='flex items-center justify-center space-x-2 cursor-pointer'>
-            <Menu className='text-gray-300 ' width={20} height={20}/>
+            {/* <Menu className='text-gray-300 ' width={20} height={20}/> */}
             <Link href="#" className='text-gray-400 font-semibold'>Rojoniaina</Link>
         </div>
         <Link href="https://github.com/Rojokabut" target="_blank" className='flex flex-row space-x-1 items-center justify-center bg-gray-800 hover:bg-gray-900 rounded-lg px-4 py-1 cursor-pointer'>
